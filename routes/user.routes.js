516427d82696e32e74f72cfaaaa73d90f98431c4
dipanshukrgit <dipanshuhub0411@gmail.com>
2025-11-07 23:19:50 +0530

    backend

---
+++ routes/user.routes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { login, signup } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.js";
+const router = express.Router();
+
+router.post("/signup", upload.single("image"), signup);
+router.post("/login", login);
+export default router;
